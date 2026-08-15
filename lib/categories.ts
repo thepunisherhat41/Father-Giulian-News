d86ab40220b7091c17798db7cacf4482c10bb043
thepunisherhat41 <thepunisherhat@gmail.com>
2026-08-15 17:03:21 -0300

    content: load 15 Aug 17h update

--- lib/categories.ts
+++ lib/categories.ts
@@ -9,6 +9,7 @@ import './daily-overrides-2026-08-15';
 import './daily-overrides-2026-08-15-fixes';
 import './vehicle-overrides';
 import './daily-overrides-2026-08-15-10h';
+import './daily-overrides-2026-08-15-17h';
 
 export type Category = {
   slug: string;
