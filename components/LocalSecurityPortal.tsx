fdf9266c4ee793cd40c3237104880b4c84f582c4
thepunisherhat41 <thepunisherhat@gmail.com>
2026-08-21 00:37:46 -0300

    use live Zona Leste current hub

--- components/LocalSecurityPortal.tsx
+++ components/LocalSecurityPortal.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
-import LocalSecurityHub from './LocalSecurityHub';
+import LocalSecurityHub from './LocalSecurityHubCurrent';
 
 export default function LocalSecurityPortal() {
   const [host, setHost] = useState<HTMLElement | null>(null);
