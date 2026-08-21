a5ca814fa44869be7cf27f6d08c180d5022498f5
thepunisherhat41 <thepunisherhat@gmail.com>
2026-08-21 10:18:00 -0300

    editorial: ativa aprofundamento revisado das 10h

--- lib/editorial-deep-read-current.ts
+++ lib/editorial-deep-read-current.ts
@@ -1,4 +1,4 @@
 // Ponteiro da camada de aprofundamento da edição ativa.
 // A automação diária troca este export após revisar os temas do dia.
-export { deepReadForSlug } from './editorial-deep-read-2026-08-21';
-export type { EditorialDeepRead } from './editorial-deep-read-2026-08-21';
+export { deepReadForSlug } from './editorial-deep-read-2026-08-21-10h';
+export type { EditorialDeepRead } from './editorial-deep-read-2026-08-21-10h';
