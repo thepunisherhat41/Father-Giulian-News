6bfc1a43f4574ddeeb73ec42fdd0065d84e69f1d
thepunisherhat41 <thepunisherhat@gmail.com>
2026-08-21 00:51:53 -0300

    add date-independent current deep-read pointer

---
+++ lib/editorial-deep-read-current.ts
@@ -0,0 +1,4 @@
+// Ponteiro da camada de aprofundamento da edição ativa.
+// A automação diária troca este export após revisar os temas do dia.
+export { deepReadForSlug } from './editorial-deep-read-2026-08-21';
+export type { EditorialDeepRead } from './editorial-deep-read-2026-08-21';
