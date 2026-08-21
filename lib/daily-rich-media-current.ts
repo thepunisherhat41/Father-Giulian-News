e8ab284a487a20db31800d19413194ac94e06e29
thepunisherhat41 <thepunisherhat@gmail.com>
2026-08-21 00:51:44 -0300

    add date-independent current media pointer

---
+++ lib/daily-rich-media-current.ts
@@ -0,0 +1,3 @@
+// Ponteiro do catálogo visual da edição ativa.
+// Atualize este arquivo junto com daily-current.ts em cada nova edição.
+export { dailyRichMedia20260821 as dailyRichMediaCurrent } from './daily-rich-media-2026-08-21';
