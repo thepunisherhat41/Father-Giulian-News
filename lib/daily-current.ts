f099689e214d94cebf1c59dc0a36fb3e7126d6e0
thepunisherhat41 <thepunisherhat@gmail.com>
2026-08-21 10:03:31 -0300

    editorial: ativa revisão das 10h de 21/08

--- lib/daily-current.ts
+++ lib/daily-current.ts
@@ -1,3 +1,4 @@
 // Ponteiro único para a edição ativa.
 // A automação diária deve alterar apenas este import depois de gerar/revisar a edição do dia.
 import './daily-overrides-2026-08-21';
+import './daily-overrides-2026-08-21-10h';
