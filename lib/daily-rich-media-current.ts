74ab37e23b8c003d1d01951ae0ea4e19b2e100ea
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-01 11:44:29 -0300

    media: prioritize 11h37 politics catalog

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,5 +1,6 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente catálogos da data corrente podem ficar ativos; revisão mais nova tem precedência.
+import {dailyRichMedia20260901_1137} from './daily-rich-media-2026-09-01-1137';
 import {dailyRichMedia20260901_0834} from './daily-rich-media-2026-09-01-0834';
 import {dailyRichMedia20260901} from './daily-rich-media-2026-09-01';
-export const dailyRichMediaCurrent=[...dailyRichMedia20260901_0834,...dailyRichMedia20260901];
+export const dailyRichMediaCurrent=[...dailyRichMedia20260901_1137,...dailyRichMedia20260901_0834,...dailyRichMedia20260901];
