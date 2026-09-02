6bfa8ccce2cf481ac7352f3ec663601dcf24283a
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-02 08:37:59 -0300

    media: prioritize 08h33 Brasil catalog

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,5 +1,6 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente catálogos da data corrente podem ficar ativos; revisão mais nova tem precedência.
+import {dailyRichMedia20260902_0833} from './daily-rich-media-2026-09-02-0833';
 import {dailyRichMedia20260902_0539} from './daily-rich-media-2026-09-02-0539';
 import {dailyRichMedia20260902} from './daily-rich-media-2026-09-02';
-export const dailyRichMediaCurrent=[...dailyRichMedia20260902_0539,...dailyRichMedia20260902];
+export const dailyRichMediaCurrent=[...dailyRichMedia20260902_0833,...dailyRichMedia20260902_0539,...dailyRichMedia20260902];
