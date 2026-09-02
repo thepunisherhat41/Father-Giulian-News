2bd9776f6f785425982dffcc1133fff9417a6b85
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-02 14:32:17 -0300

    media: prioritize 14h28 Security Briefing catalog

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,7 +1,8 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente catálogos da data corrente podem ficar ativos; revisão mais nova tem precedência.
+import {dailyRichMedia20260902_1428} from './daily-rich-media-2026-09-02-1428';
 import {dailyRichMedia20260902_1134} from './daily-rich-media-2026-09-02-1134';
 import {dailyRichMedia20260902_0833} from './daily-rich-media-2026-09-02-0833';
 import {dailyRichMedia20260902_0539} from './daily-rich-media-2026-09-02-0539';
 import {dailyRichMedia20260902} from './daily-rich-media-2026-09-02';
-export const dailyRichMediaCurrent=[...dailyRichMedia20260902_1134,...dailyRichMedia20260902_0833,...dailyRichMedia20260902_0539,...dailyRichMedia20260902];
+export const dailyRichMediaCurrent=[...dailyRichMedia20260902_1428,...dailyRichMedia20260902_1134,...dailyRichMedia20260902_0833,...dailyRichMedia20260902_0539,...dailyRichMedia20260902];
