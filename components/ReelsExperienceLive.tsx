7b9b2bbc1a2dd267548229dd2e1d129d974e6fc8
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-04 14:11:58 -0300

    feed: apply 14h Corinthians patch after morning patches

--- components/ReelsExperienceLive.tsx
+++ components/ReelsExperienceLive.tsx
@@ -4,11 +4,13 @@ import {curiosityCollections} from '@/lib/curiosity-collections';
 import {applyCurrentCuriosityRotation} from '@/lib/current-curiosity-rotation';
 import {applyCurrentReelPatches20260904} from '@/lib/current-reel-patches-2026-09-04';
 import {applyCurrentReelPatches202609040528} from '@/lib/current-reel-patches-2026-09-04-0528';
+import {applyCurrentReelPatches202609041408} from '@/lib/current-reel-patches-2026-09-04-1408';
 import ReelsExperienceV27 from './ReelsExperienceV27';
 
 export default function ReelsExperienceLive(){
   applyCurrentCuriosityRotation(curiosityCollections);
   applyCurrentReelPatches20260904(dailyContent);
   applyCurrentReelPatches202609040528(dailyContent);
+  applyCurrentReelPatches202609041408(dailyContent);
   return <ReelsExperienceV27/>;
 }
