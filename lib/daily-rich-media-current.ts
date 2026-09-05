cadf13a03aac9311f9927470623c24dbfb767eb1
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-05 00:01:51 -0300

    media: prioritize 23h58 curiosity catalog

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,9 +1,10 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente catálogos da data corrente podem ficar ativos; revisão mais nova tem precedência.
+import {dailyRichMedia202609042358} from './daily-rich-media-2026-09-04-2358';
 import {dailyRichMedia202609042034} from './daily-rich-media-2026-09-04-2034';
 import {dailyRichMedia202609041805} from './daily-rich-media-2026-09-04-1805';
 import {dailyRichMedia202609041747} from './daily-rich-media-2026-09-04-1747';
 import {dailyRichMedia202609041408} from './daily-rich-media-2026-09-04-1408';
 import {dailyRichMedia202609040528} from './daily-rich-media-2026-09-04-0528';
 import {dailyRichMedia20260904} from './daily-rich-media-2026-09-04';
-export const dailyRichMediaCurrent=[...dailyRichMedia202609042034,...dailyRichMedia202609041805,...dailyRichMedia202609041747,...dailyRichMedia202609041408,...dailyRichMedia202609040528,...dailyRichMedia20260904];
+export const dailyRichMediaCurrent=[...dailyRichMedia202609042358,...dailyRichMedia202609042034,...dailyRichMedia202609041805,...dailyRichMedia202609041747,...dailyRichMedia202609041408,...dailyRichMedia202609040528,...dailyRichMedia20260904];
