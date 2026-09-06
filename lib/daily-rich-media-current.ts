9bcfb0430024882e6069b819520d44b4031cdfa2
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-06 19:19:46 -0300

    06/09 19h17: prioriza fotos temáticas

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,8 +1,9 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente catálogos da data corrente podem ficar ativos.
+import {dailyRichMedia202609061917} from './daily-rich-media-2026-09-06-1917';
 import {dailyRichMedia202609061756} from './daily-rich-media-2026-09-06-1756';
 import {dailyRichMedia202609061415} from './daily-rich-media-2026-09-06-1415';
 import {dailyRichMedia202609061147} from './daily-rich-media-2026-09-06-1147';
 import {dailyRichMedia202609060831} from './daily-rich-media-2026-09-06-0831';
 import {dailyRichMedia20260906} from './daily-rich-media-2026-09-06';
-export const dailyRichMediaCurrent=[...dailyRichMedia202609061756,...dailyRichMedia202609061415,...dailyRichMedia202609061147,...dailyRichMedia202609060831,...dailyRichMedia20260906];
+export const dailyRichMediaCurrent=[...dailyRichMedia202609061917,...dailyRichMedia202609061756,...dailyRichMedia202609061415,...dailyRichMedia202609061147,...dailyRichMedia202609060831,...dailyRichMedia20260906];
