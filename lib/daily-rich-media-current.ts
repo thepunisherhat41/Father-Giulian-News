dbdae78bb2a1f03949040aee95fb739d4e0582c1
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-06 08:36:11 -0300

    feat: prioritize 08:31 Kyiv media

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,4 +1,5 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente catálogos da data corrente podem ficar ativos.
+import {dailyRichMedia202609060831} from './daily-rich-media-2026-09-06-0831';
 import {dailyRichMedia20260906} from './daily-rich-media-2026-09-06';
-export const dailyRichMediaCurrent=[...dailyRichMedia20260906];
+export const dailyRichMediaCurrent=[...dailyRichMedia202609060831,...dailyRichMedia20260906];
