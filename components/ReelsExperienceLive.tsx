e86ba9db93eba2debfbb396901e88898d32cefb1
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-07 06:06:43 -0300

    feed: aplica patch 07/09 06h03

--- components/ReelsExperienceLive.tsx
+++ components/ReelsExperienceLive.tsx
@@ -22,6 +22,7 @@ import {applyCurrentReelPatches202609061756} from '@/lib/current-reel-patches-20
 import {applyCurrentReelPatches202609061917} from '@/lib/current-reel-patches-2026-09-06-1917';
 import {applyCurrentReelPatches202609062232} from '@/lib/current-reel-patches-2026-09-06-2232';
 import {applyCurrentReelPatches20260907} from '@/lib/current-reel-patches-2026-09-07';
+import {applyCurrentReelPatches202609070603} from '@/lib/current-reel-patches-2026-09-07-0603';
 import ReelsExperienceV27 from './ReelsExperienceV27';
 
 export default function ReelsExperienceLive(){
@@ -34,7 +35,6 @@ export default function ReelsExperienceLive(){
   applyCurrentReelPatches202609042034(dailyContent);
   applyCurrentReelPatches20260905(dailyContent);
   applyCurrentReelPatches202609051449(dailyContent);
-  applyCurrentReelPatches202609051749(dailyContent);
   applyCurrentReelPatches202609052013(dailyContent);
   applyCurrentReelPatches202609052306(dailyContent);
   applyCurrentReelPatches20260906(dailyContent);
@@ -46,5 +46,6 @@ export default function ReelsExperienceLive(){
   applyCurrentReelPatches202609061917(dailyContent);
   applyCurrentReelPatches202609062232(dailyContent);
   applyCurrentReelPatches20260907(dailyContent);
+  applyCurrentReelPatches202609070603(dailyContent);
   return <ReelsExperienceV27/>;
 }
