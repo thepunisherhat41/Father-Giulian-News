70f40217fe078dfe1b220f575fdff29b69724bce
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-07 00:07:22 -0300

    edition: aponta mídia corrente para 07/09

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,10 +1,4 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente catálogos da data corrente podem ficar ativos.
-import {dailyRichMedia202609062232} from './daily-rich-media-2026-09-06-2232';
-import {dailyRichMedia202609061917} from './daily-rich-media-2026-09-06-1917';
-import {dailyRichMedia202609061756} from './daily-rich-media-2026-09-06-1756';
-import {dailyRichMedia202609061415} from './daily-rich-media-2026-09-06-1415';
-import {dailyRichMedia202609061147} from './daily-rich-media-2026-09-06-1147';
-import {dailyRichMedia202609060831} from './daily-rich-media-2026-09-06-0831';
-import {dailyRichMedia20260906} from './daily-rich-media-2026-09-06';
-export const dailyRichMediaCurrent=[...dailyRichMedia202609062232,...dailyRichMedia202609061917,...dailyRichMedia202609061756,...dailyRichMedia202609061415,...dailyRichMedia202609061147,...dailyRichMedia202609060831,...dailyRichMedia20260906];
+import {dailyRichMedia20260907} from './daily-rich-media-2026-09-07';
+export const dailyRichMediaCurrent=[...dailyRichMedia20260907];
