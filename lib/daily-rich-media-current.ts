8dc7a3376be1dfaebf5941afbc42473661639bc9
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-07 11:38:51 -0300

    media: prioriza catálogo intradiário 11h35

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,5 +1,6 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente catálogos da data corrente podem ficar ativos.
+import {dailyRichMedia202609071135} from './daily-rich-media-2026-09-07-1135';
 import {dailyRichMedia202609070810} from './daily-rich-media-2026-09-07-0810';
 import {dailyRichMedia20260907} from './daily-rich-media-2026-09-07';
-export const dailyRichMediaCurrent=[...dailyRichMedia202609070810,...dailyRichMedia20260907];
+export const dailyRichMediaCurrent=[...dailyRichMedia202609071135,...dailyRichMedia202609070810,...dailyRichMedia20260907];
