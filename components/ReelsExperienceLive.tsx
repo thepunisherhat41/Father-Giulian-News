3fcdd0ac7b407b7271cd0b26f94c658051f2264e
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-08 02:48:52 -0300

    wire: ativa patch intradiário de 08/09 02h46

--- components/ReelsExperienceLive.tsx
+++ components/ReelsExperienceLive.tsx
@@ -29,6 +29,7 @@ import {applyCurrentReelPatches202609071723} from '@/lib/current-reel-patches-20
 import {applyCurrentReelPatches202609072011} from '@/lib/current-reel-patches-2026-09-07-2011';
 import {applyCurrentReelPatches202609072351} from '@/lib/current-reel-patches-2026-09-07-2351';
 import {applyCurrentReelPatches20260908} from '@/lib/current-reel-patches-2026-09-08';
+import {applyCurrentReelPatches202609080246} from '@/lib/current-reel-patches-2026-09-08-0246';
 import ReelsExperienceV27 from './ReelsExperienceV27';
 
 export default function ReelsExperienceLive(){
@@ -60,5 +61,6 @@ export default function ReelsExperienceLive(){
   applyCurrentReelPatches202609072011(dailyContent);
   applyCurrentReelPatches202609072351(dailyContent);
   applyCurrentReelPatches20260908(dailyContent);
+  applyCurrentReelPatches202609080246(dailyContent);
   return <ReelsExperienceV27/>;
 }
