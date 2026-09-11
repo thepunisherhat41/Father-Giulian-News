3e00c44301bb41732ce9038086bb0e575d2216c7
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-11 11:26:38 -0300

    wire 11/09 11h23 media catalog

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,6 +1,7 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente catálogos da data corrente podem ficar ativos.
+import {dailyRichMedia202609111123} from './daily-rich-media-2026-09-11-1123';
 import {dailyRichMedia202609110851} from './daily-rich-media-2026-09-11-0851';
 import {dailyRichMedia202609110553} from './daily-rich-media-2026-09-11-0553';
 import {dailyRichMedia20260911} from './daily-rich-media-2026-09-11';
-export const dailyRichMediaCurrent=[...dailyRichMedia202609110851,...dailyRichMedia202609110553,...dailyRichMedia20260911];
+export const dailyRichMediaCurrent=[...dailyRichMedia202609111123,...dailyRichMedia202609110851,...dailyRichMedia202609110553,...dailyRichMedia20260911];
