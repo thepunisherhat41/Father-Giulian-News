440a8898497ee997b3c10925edb884604e4088e0
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-12 20:37:53 -0300

    Father News 12/09 20h32 · prioritize current media

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,8 +1,9 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente catálogos da data corrente podem ficar ativos.
+import {dailyRichMedia202609122032} from './daily-rich-media-2026-09-12-2032';
 import {dailyRichMedia202609121717} from './daily-rich-media-2026-09-12-1717';
 import {dailyRichMedia202609121114} from './daily-rich-media-2026-09-12-1114';
 import {dailyRichMedia202609120842} from './daily-rich-media-2026-09-12-0842';
 import {dailyRichMedia202609120550} from './daily-rich-media-2026-09-12-0550';
 import {dailyRichMedia20260912} from './daily-rich-media-2026-09-12';
-export const dailyRichMediaCurrent=[...dailyRichMedia202609121717,...dailyRichMedia202609121114,...dailyRichMedia202609120842,...dailyRichMedia202609120550,...dailyRichMedia20260912];
+export const dailyRichMediaCurrent=[...dailyRichMedia202609122032,...dailyRichMedia202609121717,...dailyRichMedia202609121114,...dailyRichMedia202609120842,...dailyRichMedia202609120550,...dailyRichMedia20260912];
