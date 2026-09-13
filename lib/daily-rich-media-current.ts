10323cf2ce5dad837dd354792a79c4507f992810
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-13 01:12:17 -0300

    feat: ponteiro de mídia para 13/09 real

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,10 +1,4 @@
 // Ponteiro do catálogo visual da edição ativa.
-// HARD GATE: somente catálogos da data corrente podem ficar ativos.
-import {dailyRichMedia202609122338} from './daily-rich-media-2026-09-12-2338';
-import {dailyRichMedia202609122032} from './daily-rich-media-2026-09-12-2032';
-import {dailyRichMedia202609121717} from './daily-rich-media-2026-09-12-1717';
-import {dailyRichMedia202609121114} from './daily-rich-media-2026-09-12-1114';
-import {dailyRichMedia202609120842} from './daily-rich-media-2026-09-12-0842';
-import {dailyRichMedia202609120550} from './daily-rich-media-2026-09-12-0550';
-import {dailyRichMedia20260912} from './daily-rich-media-2026-09-12';
-export const dailyRichMediaCurrent=[...dailyRichMedia202609122338,...dailyRichMedia202609122032,...dailyRichMedia202609121717,...dailyRichMedia202609121114,...dailyRichMedia202609120842,...dailyRichMedia202609120550,...dailyRichMedia20260912];
+// HARD GATE: somente o catálogo da data corrente pode ficar ativo.
+import {dailyRichMedia20260913} from './daily-rich-media-2026-09-13';
+export const dailyRichMediaCurrent=[...dailyRichMedia20260913];
