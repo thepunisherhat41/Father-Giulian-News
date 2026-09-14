da96d96bd44986ce76445dfbf88ccc3805887fd3
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-13 23:51:31 -0300

    media: prioritize late 13/09 World catalog

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,4 +1,5 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente o catálogo da data corrente pode ficar ativo.
 import {dailyRichMedia20260913} from './daily-rich-media-2026-09-13';
-export const dailyRichMediaCurrent=[...dailyRichMedia20260913];
+import {dailyRichMedia202609132346} from './daily-rich-media-2026-09-13-2346';
+export const dailyRichMediaCurrent=[...dailyRichMedia202609132346,...dailyRichMedia20260913];
