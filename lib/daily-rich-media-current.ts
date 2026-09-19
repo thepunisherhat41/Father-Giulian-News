20a9e1b2cb3ab20343326756fc843f4987fb4a99
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-19 02:36:43 -0300

    media: activate 19/09 catalog

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,4 +1,4 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente o catálogo da data corrente pode ficar ativo.
-import {dailyRichMedia20260918} from './daily-rich-media-2026-09-18';
-export const dailyRichMediaCurrent=[...dailyRichMedia20260918];
+import {dailyRichMedia20260919} from './daily-rich-media-2026-09-19';
+export const dailyRichMediaCurrent=[...dailyRichMedia20260919];
