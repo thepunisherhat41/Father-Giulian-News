870b5a2a3803129f628e79d563e81d9084f79766
thepunisherhat41 <thepunisherhat@gmail.com>
2026-09-20 01:45:21 -0300

    chore: point active media to 20 Sep

--- lib/daily-rich-media-current.ts
+++ lib/daily-rich-media-current.ts
@@ -1,4 +1,4 @@
 // Ponteiro do catálogo visual da edição ativa.
 // HARD GATE: somente o catálogo da data corrente pode ficar ativo.
-import {dailyRichMedia20260919} from './daily-rich-media-2026-09-19';
-export const dailyRichMediaCurrent=[...dailyRichMedia20260919];
+import {dailyRichMedia20260920} from './daily-rich-media-2026-09-20';
+export const dailyRichMediaCurrent=[...dailyRichMedia20260920];
